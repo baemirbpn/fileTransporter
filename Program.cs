81585620f568c97f68644da9f16c26484841ae56
baemirbpn <baemir@bpn.com.tr>
2024-04-29 21:20:35 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Kaynak dizinleri
+        string[] sourceDirectories =
+        {
+            @"C:\Walletgate\Master.Api",
+            @"C:\Walletgate\Common.Api",
+            @"C:\Walletgate\InvoicePayment.Backoffice"
+        };
+
+        // Hedef dizin
+        string destinationBaseDirectory = @"C:\Users\baemir\Documents\AppSettings-Tracker\appSettingsChecker\Volume";
+
+        foreach (string sourceDir in sourceDirectories)
+        {
+            // appsettings.json ve appsettings.Development.json dosyalarını kopyala
+            string[] sourceFiles = Directory.GetFiles(sourceDir, "appsettings*.json");
+
+            foreach (string sourceFile in sourceFiles)
+            {
+                string fileName = Path.GetFileName(sourceFile);
+                string sourceDirectoryName = new DirectoryInfo(sourceDir).Name;
+
+                string destinationFile = Path.Combine(destinationDirectoryName, fileName);
+
+                try
+                {
+                    // Hedef klasörü oluştur (eğer zaten varsa, bu adımı atla)
+                    Directory.CreateDirectory(destinationDirectoryName);
+
+                    // Dosyayı kopyala ve üzerine yaz
+                    File.Copy(sourceFile, destinationFile, true);
+                    Console.WriteLine($"Dosya {sourceFile} {destinationFile} dizinine kopyalandı.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Hata: {ex.Message}");
+                }
+            }
+        }
+    }
+}
